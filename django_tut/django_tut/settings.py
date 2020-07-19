ae1ce4dc9b51013c2e4b5273fc896da232fa2450
Kartik Gokte <ksgokte@gmail.com>
2020-07-19 22:39:30 +0530

    Added model and view for poll app

--- django_tut/django_tut/settings.py
+++ django_tut/django_tut/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['192.168.29.243', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
