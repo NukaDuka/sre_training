15d39f1e7688335f3f82ee08507e0e55791fb881
Kartik Gokte <ksgokte@gmail.com>
2020-07-20 11:30:53 +0530

    Added detailed views

--- django_tut/polls/views.py
+++ django_tut/polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world')
+    qlist = Question.objects.order_by('id')
+    context = {
+                'qlist': qlist,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 
@@ -12,3 +19,17 @@ def secret(request):
     return HttpResponse('The secret code is ' + str(secret.i))
 
 secret.i = 0
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question no. " + str(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You're now voting on question no. " + str(question_id))
