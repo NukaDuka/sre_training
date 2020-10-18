3006d230bd7bca1bd67e70456b2b1fc5d13f0325
Kartik Gokte <ksgokte@gmail.com>
2020-10-18 17:54:49 +0530

    Changed nginx docker configs

--- nginx_tut/nginx_custom/run.sh
+++ nginx_tut/nginx_custom/run.sh
@@ -1,1 +1,2 @@
-sudo docker run -d --rm --name nginx-custom -p 8080:80 -p 8081:81 --health-cmd='curl -s localhost:81 || exit 1' --health-interval=10s  --health-start-period=5s nukaduka1/nginx_custom:latest
+sudo docker rm --force nginx-custom
+sudo docker run -d --name nginx-custom -p 8080:80 -p 8081:81 --health-cmd='curl -s localhost:81 || exit 1' --health-interval=10s  --health-start-period=5s nukaduka1/nginx_custom:latest
