9de57b19b2521171ee7500215e0aa553f5b0f384
Kartik Gokte <ksgokte@gmail.com>
2020-11-15 20:19:40 +0530

    Changed sync script

--- php_tut/sync.sh
+++ php_tut/sync.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 # Intended to be run from within the code folder
-sudo cp -R . /var/lib/docker/volumes/xampp-pma-volume/_data/code
-sudo rm /var/lib/docker/volumes/xampp-pma-volume/_data/code/sync
+sudo cp -R /mnt/c/Users/kartik.go/Desktop/Projects/sre_training/php_tut/sync.sh /var/lib/docker/volumes/xampp-pma-volume/_data/code
