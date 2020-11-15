2f7378f0af2d26155c07f4e271323a605ddc92f8
Kartik Gokte <ksgokte@gmail.com>
2020-11-15 20:22:18 +0530

    Fixed script

--- php_tut/sync.sh
+++ php_tut/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Intended to be run from within the code folder
-sudo cp -R /mnt/c/Users/kartik.go/Desktop/Projects/sre_training/php_tut/sync.sh /var/lib/docker/volumes/xampp-pma-volume/_data/code
+sudo cp -R /mnt/c/Users/kartik.go/Desktop/Projects/sre_training/php_tut/code /var/lib/docker/volumes/xampp-pma-volume/_data/code
