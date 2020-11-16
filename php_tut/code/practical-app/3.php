28325c7c103b52a3dab1564c831307e5e08ce05c
Kartik Gokte <ksgokte@gmail.com>
2020-11-16 10:52:50 +0530

    Added ex3 code

--- php_tut/code/practical-app/3.php
+++ php_tut/code/practical-app/3.php
@@ -6,26 +6,41 @@
 	<aside class="col-xs-4">
 
 	<?php Navigation();?>
-			
+
 	</aside><!--SIDEBAR-->
 
 
 <article class="main-content col-xs-8">
 
-<?php  
+<?php
 
-/*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
+	if (1 > 2) {
+		echo "1 GREATER THAN 2? HOW???";
+	}
+	elseif (3 >= 4) {
+		echo "3 GREATER THAN/EQUAL TO 4???? IMPOSSIBLE!";
+	}
+	else {
+		echo "<h1>I love PHP</h1>"
+		echo "<h6>kapp</h6>"
+	}
 
+	for ($i = 1; $i < 10; $i++) {
+		echo $i . "<br>";
+	}
 
+	$number = 10
+	switch ($number) {
+		case 0:
+			echo "The number is 0";
+			break;
+		case 10:
+			echo "The number is 10";
+		default:
+			echo "Bottom text"
+			break;
+	}
 
-	Step 2: Make a forloop  that displays 10 numbers
-
-
-	Step 3 : Make a switch Statement that test againts one condition with 5 cases
-
- */
-
-	
 ?>
 
 
@@ -34,5 +49,5 @@
 
 
 </article><!--MAIN CONTENT-->
-	
+
 <?php include "includes/footer.php" ?>
