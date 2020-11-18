5e0adeefed7dc59d992670c34a7c86a4110d9a2c
Kartik Gokte <ksgokte@gmail.com>
2020-11-18 13:03:27 +0530

    Changed syntax

--- php_tut/code/scuffed_parabola.php
+++ php_tut/code/scuffed_parabola.php
@@ -11,12 +11,12 @@
 $param1 = rand(-1, 10);
 $param2 = rand(-1, 10);
 $param3 = rand(1, 20);
-function nextp($old_x) {
+function nextp($old_x, $param1, $param2, $param3) {
     return ($param1 * pow($old_x, 2)) + ($param2 * $old_x) + $param3;
 }
 $i = -20;
 for ($i; $i <= 20; $i++) {
-    $y = (int) ceil(nextp($i) / 10);
+    $y = (int) ceil(nextp($i, $param1, $param2, $param3) / 10);
     $j = 1;
     for ($j; $j <= $y; $j++) echo "+";
     echo "<br>";
