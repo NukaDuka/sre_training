2153ef56a89d673bb7217c54ccfcd3197eec06cb
Kartik Gokte <ksgokte@gmail.com>
2020-11-18 11:57:24 +0530

    Added comment

--- php_tut/code/practical-app/4.php
+++ php_tut/code/practical-app/4.php
@@ -21,6 +21,7 @@
 		return $num1 + $num2;
 	}
 
+	// can't be bothered to test with negative numbers LOL
 	function slow_multiply($num1, $num2) {
 		if ($num2 == 0) return 0;
 		if ($num2 == 1) return $num1;
