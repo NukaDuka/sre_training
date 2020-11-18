38c67f33b40f4c493fd6bdfff5ba1768bf072906
Kartik Gokte <ksgokte@gmail.com>
2020-11-18 11:54:51 +0530

    Added ex4

--- php_tut/code/practical-app/4.php
+++ php_tut/code/practical-app/4.php
@@ -6,24 +6,37 @@
 	<aside class="col-xs-4">
 
 		<?php Navigation();?>
-			
-		
+
+
 	</aside><!--SIDEBAR-->
 
 
 <article class="main-content col-xs-8">
 
-	
-	<?php  
 
-/*  Step1: Define a function and make it return a calculation of 2 numbers
-
-	Step 2: Make a function that passes parameters and call it using parameter values
+	<?php
 
 
- */
+	function add($num1, $num2) {
+		return $num1 + $num2;
+	}
 
-	
+	function slow_multiply($num1, $num2) {
+		if ($num2 == 0) return 0;
+		if ($num2 == 1) return $num1;
+		$sum_ = 0;
+		$num = 0;
+		for ($num; $num < $num2; $num++) {
+			$sum = add($sum, $num1);
+		}
+		return $sum;
+	}
+
+	echo slow_multiply(1, 0);
+	echo slow_multiply(10, 1);
+	echo slow_multiply(45, 2);
+	echo slow_multiply(34, 34);
+
 ?>
 
 
