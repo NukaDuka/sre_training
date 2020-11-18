b911e5e955450a5d4fd12391fa86c6a8c1cba5a7
Kartik Gokte <ksgokte@gmail.com>
2020-11-18 13:01:55 +0530

    Fixed syntax

--- php_tut/code/scuffed_parabola.php
+++ php_tut/code/scuffed_parabola.php
@@ -8,9 +8,9 @@
 <body>
 <?php
 
-$param1 = rand(-1, 10)
-$param2 = rand(-1, 10)
-$param3 = rand(1, 20)
+$param1 = rand(-1, 10);
+$param2 = rand(-1, 10);
+$param3 = rand(1, 20);
 function nextp($old_x) {
     return ($param1 * pow($old_x, 2)) + ($param2 * $old_x) + $param3;
 }
