8c558733de6443b692722d817c561add7ac5037a
Kartik Gokte <ksgokte@gmail.com>
2020-11-18 12:44:48 +0530

    Changed params

--- php_tut/code/scuffed_parabola.php
+++ php_tut/code/scuffed_parabola.php
@@ -8,9 +8,9 @@
 <body>
 <?php
 
-#y = 3x^2+2x+1
+#y = 3x^2+2x+35
 function nextp($old_x) {
-    return (3 * pow($old_x, 2)) + (2 * $old_x) + 1;
+    return (3 * pow($old_x, 2)) + (2 * $old_x) + 35;
 }
 $i = -20;
 for ($i; $i <= 20; $i++) {
