d92c847ad019863e0c173cb14909c97c859cd0b0
Kartik Gokte <ksgokte@gmail.com>
2020-11-24 10:47:21 +0530

    Added CSS

--- php_tut/code/form.php
+++ php_tut/code/form.php
@@ -2,6 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <style>
+        html,
+        body {
+            height: 100%
+        }
+    </style>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
     <link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet" />
