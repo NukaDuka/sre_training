480aeede34e280ee824c15ec8968552a26b69f90
Kartik Gokte <ksgokte@gmail.com>
2020-11-25 13:13:09 +0530

    Added ext form code

---
+++ php_tut/code/ext_form.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+        }
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+    <link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet" />
+    <script href="bootstrap/js/bootstrap.js" rel="stylesheet"></script>
+    <script href="bootstrap/js/jquery.js" rel="stylesheet"></script>
+    <title>Cool form B)</title>
+</head>
+<body>
+    <div class="d-flex justify-content-md-center align-items-center vh-100">
+        <div>
+            <h3 class="display-4">Send a message to the abyss!</h3>
+            <hr class="my-4">
+            <form action="ext_form_result.php" method="post" autocomplete="off">
+                <div class="form-group">
+                    <label for="input">Enter message:</label>
+                    <input type="text" name="input" id="input" placehold="input" autofocus class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="submit" value="Send a̸̰̎̉̈́̀̎͊̿̃̾̈ m̵̢̢̛̫͇̱͕̩͔͔͎̞͔̻͈̙̞̹͎̒̓̑̿͗̎̽̇̆̀͆̏̏̏̀̈́̔͑̍͐͒̏̿͑̾͘̚̚͠͝͝ͅḙ̵̢̡̧̜̝͍̣̠̱̪̥̘̳̹̭̝̺̪͇͔̯̦̺̗̞͕̜͇͊͂ͅṡ̵̢̧̢̹͚̘͇̭͎̗̗̠͍̩̣̲͇̹̙̘̫̤̱̅̈́̀̒̀̈̂̃̎̇͛̓̄̕̚͜͠ͅs̸̡̛̛̞̖̦͖͋̀͂̿̇̽̈́́̔͌͊͗̿̀̏͂̐̎́̀̀̽͆̈́̋̂̕͘̚ͅȧ̸̡̧̼̩̦̖̦͈͔̯͓͚͉̗͔̯͖̝͇̥̖͎̟͓̝̗̓̂͛̔͂̆̊̐̀̏̆̽̽̀̓̓̇̍͋͛̌̽̊͛̑͘̕̚̚̚̚͝͝ͅͅg̴̨̪͎̤̞̹̞̫̬̟̙̮̲̙̦̔̈̀͠ḗ̷̡̢̧͍͚͉̺̜̱̻̯̤̹͙̝̭̣̖̹͚͕̳̤̗͈͒̑̓͗̏̿͋̊̓͒̾̆̊̍͐̀́̇͌͂͗̓̈́͛͘͝ͅͅͅ ̴̡̢̝̱̞̜͍̺̠͖̞̪̞̺̮̻̭̠̫͗̑͂̔̈̃̎̃̈́̋̋͗̑͊̅̇̏̈́̎̓̕̕̕͜͝ţ̸̮̺̘̗͕́́̒͊̓͒̀̒̒͊͑̒́͒̔̀͗̌̌̉͐̾̔̚̚̚͝͝͠ö̷̧̢̢̯̠̠̺̠͈̺̬̺͕̪̻̺̟͇̖̭̩̞̠̩̼̤̪͗́͋̊̈́̂̈́͛̄͌͑͆͂̀̒̓͒͛͂͋̚̕͜͠͠͝ͅ t̷̢̡̧̧̛̻͖̗̼̠̦̱͓͍͎͕͕̱̯̭̯͈̝̖̱̳͎͍̩̼̟͈̲̣̳̫̘̱̟̙̮͉̺͉̻̮͕̯͙͇̠̝̗̪̜̲̳̰̱̗͈̜̦̮͓̝̫͕̥͎̭͕̯̲̏̆̇̂̃̿̅̉͊̿̃̌̀͛͂̾͘̚̕͜͜ͅͅͅͅͅh̸̢̧̢̛̯̥̱̗̭̜̼͖̤̮͖͎̝̟̱̓̃͗͊̏̓̉͂̋͒͛͂̑͂̔̉̽̈͑̒͆̄͋̓͗̈͑́̉̂̉̍́̽̃́͒̐̽́̈́͛̋̾̀́̈́͆͂̀̍͌̈́̓͐͑͛̾͊̒̀̔̂̄̔͌͒͘̕͜͝͠͠͝͝e̵̢̨̧̡̨̨̨̡̢̛̝̟͚̲̺̞̞̲̞̹͕͎͖̯̖̺̣͉̮̻̮͎̰̙̼̤͙̦͓͕̘̯̤̺̙̳̝̼̻̫̣̰̖̯̯̹̖̻̻̲͉̱̭̰̪̺̘̣̭̞̳̓͑̇̋̌̂̈̆̑̂̒̈́̿̆̉̂̏̎́̊̎͗͒̽̎̐́͛͑̽̈̌̽́̎̋̊́́͂̈́̀͋͋̃̾̑͛̔̓̽̃͆͂͌̊͑̅̉́̒̉͛͐͋͛̆̍͆͗̏̂͆̒̓̊̓̇̊̒͒̓̈́̑̕̚͘͘̕̕͜͜͜͝͝͠͝͝͠͠͠ͅͅͅͅ ̶̢̡̢̢̨̨̢̰̼̼͇̙̳̮̼͓͖̪̠̪̦̯̤̯̥̥̰̮̩͍͉̗͖̫̘̳̘̗̼̼̜͙̹̲̬͙̙̱̭̖̬̝̤͇̜̗̠̩̗͖̹̫͉̦͉͍̩̝̣̘͖͇͔̗̭̣̘͖̞͚̲̲̞͓̳͍̳̦̣̗̈́̿̐̋̎̉̎̐͘̚͜͜͜ͅv̵̨̨̨̧̨̛̛̛̛͔̫̭̜̰̼͍̞͍̙̹̟̣̦̱͍̝̪̬̗̬̣̦͇͚̱̙̱̭͇̞̻̠̳̦̼̭͎̹̜͔̱͓̬͕̭̫͇̖̭͓͉͍̫͖̼̜̞̠̘͈̩̟̬͓̙̻̗̘̘̹͐͒̾̃̃͂̉͊͊͐͊̾̀̏͒̾̀̂͗̋́̃͌̔̇̄͌̓̑͌͊͒̓͂͂̐̀̊̊̓̄̎̔̐̀̓̌̿͒́̓̅͊̄̋̒̀̌͒̈́͋̈́̆̽̓̽̍̎̀̋̓̾̋͛̒̑̚͘͘̕̚̕͘͘͘͜͜͜͜͝͝͝͝͝͝͝ͅͅơ̶̢̜̰̬̻͔̜͕̭̤̜̻̫̬͖̟͍̖̮̟̭̮̠̲̖̹̳̯̦̥̩̭͕̯͎͚͕͉̯͓̤̣̺̭̫̦̘̤̰͓̱̰͖̝̒͂̒̂̈́̅͂̓̇̿̊̀͒̔̊̾̂́͊͒̈́̇͊̽̒̕̚̚͘͘͜ͅi̸̧̨̨̨̨̧̡̛̛̺͍̦͎͇̻̩̣̖͔͎̦̺̘̩̣̝̱̹̠̲̮͉͖͙̪͇̹̻̮̭̝̞͉̰̤͉̘͕̦̮͓̣̪̩͕͓̣̼͎̲̳͇̪͔̠̭̳̹̊̑̾̃́̉̀̊͑̐͗̎̆́̃͗͊͐̎̃́̅̐̌͒̅̆́̃̕̚̚̚͜͜͠͝͠͝d̶̨̡̧̧̨̢̛̲̗̼̥̥̣̭̼͕̘̟̝̘̪̣͔̭͙̯̭͓̣̖̤͍̳͚̜̱̹͖̳̹̪̟̙̳͕͕̪͚̬̰̝̩̥̻͖̥̭̭͓̘͓̥͔͎͍͈̬̬̲̟̭̤̱̰̮̹̹̻̖̮̹̯͇͓͓̹͇̗̐̄̍́̋̈́́̈́͊̔͘͜͜͜͝ͅ" class="btn btn-primary">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ php_tut/code/ext_form_result.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+            background-color: red;
+            color: white;
+        }
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+    <link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet" />
+    <script href="bootstrap/js/bootstrap.js" rel="stylesheet"></script>
+    <script href="bootstrap/js/jquery.js" rel="stylesheet"></script>
+    <title>ţ̵̡̱̱͖̌̆͒͛̀̑̅̍̄͛͝h̸͉̗̖̞̥͉͔̻̏ḛ̷̘̗̙͛̏̇̿̽̿̋͜͠ ̷̬̭̪̻̠͍̱̩̟̞̑̄̕͝͠ͅv̸̡̺̟̟̬̪̰̎̀̃̂̌͌̏o̸̡̜̱̒̿̈͑̏̆͘̚͘i̴̧͖̖̯̓͋̈̿̔́̑̿ͅd̴͉̹͚̝̿</title>
+</head>
+<body>
+    <div class="d-flex justify-content-md-center align-items-center vh-100">
+        <?php
+            if (isset($_POST["input"])) {
+                echo '<h1 class="display-4"> Your message was: "' . $_POST["input"] . '"</h1>';
+                echo '<h1 class="display-4">T̶͇̩̘̽̓̀̅̔̏̀̿H̴̟̟̆́̅̓́̿̔̾́̅̂̚E̶͇͈̫͔̥̭͓͛̑̍͆̒̾̔̂̐̊͋̈́ ̷̟̫͍̭̗̣͙̟̺̀̾̓̈́̊̅͂̒́̓̋V̶̟̬̻͛̎̍̽̐͝O̷͓̗͈̭̩͈͔͓͓̒͗̏̎̀̒̀̀͝Ȉ̵̛̖̣̻͔͎͉͊̈́͒̍̚D̷̨̛̜͍̝̯͕͗̊̊̋̚ͅ ̴̢̨͖̬̩̒̆̓̅̓̋̇Ṡ̴̛̬͙͇̍́̒̿̐͊̋̃͠͝A̴̛̲͓̪͉͖̭̫Y̵̠̤̣̦̹̍́Ŝ̴̢̪̲̩̱͇̈̆ ̴̢̧̧̭͎̟̥̼̄̾͛͒́͒͝Ĥ̴̢͕̱̽̓̿͊̒͒̏È̶͎̦L̶͈̪͇̜̬̇̈́͘L̸̯̗̱̅Ö̵̩̬́͜͝ </h1>';
+            }
+        ?>
+    </div>
+</body>
+</html>
