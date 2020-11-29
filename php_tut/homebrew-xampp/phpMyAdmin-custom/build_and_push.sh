f8c64d16f821a28ed64eed60c511906df244c70a
Kartik Gokte <ksgokte@gmail.com>
2020-11-29 17:45:24 +0530

    Added arm dockerfiles

--- php_tut/homebrew-xampp/phpMyAdmin-custom/build_and_push.sh
+++ php_tut/homebrew-xampp/phpMyAdmin-custom/build_and_push.sh
@@ -1,3 +1,4 @@
 docker build -t nukaduka1/custom-xampp .
+docker tag nukaduka1/custom-xampp nukaduka1/custom-xampp:arm
 docker login
-docker push nukaduka1/custom-xampp
+docker push nukaduka1/custom-xampp:arm
