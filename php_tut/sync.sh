5e8d97244cb5358591fa499513bd4c8e469449ed
Kartik Gokte <ksgokte@gmail.com>
2020-12-10 01:07:04 +0530

    Added debug to sync script

--- php_tut/sync.sh
+++ php_tut/sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Intended to be run from within the code folder
 set -ex
+echo $PWD
 docker run -d --rm --name dummy -v xampp-pma-volume:/root alpine tail -f /dev/null
 docker cp ./code dummy:/root/
 docker stop dummy
