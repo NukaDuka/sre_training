f244df96e25326dbeea5b286b58268fe0d96e9f6
Kartik Gokte <ksgokte@gmail.com>
2020-12-11 11:46:17 +0530

    Updated sync script

--- php_tut/sync.sh
+++ php_tut/sync.sh
@@ -4,5 +4,5 @@ set -ex
 echo $PWD
 docker run -d --rm --name dummy -v xampp-pma-volume:/root alpine tail -f /dev/null
 docker cp ./code dummy:/root/
-docker exec -it dummy touch "/root/$(git rev-parse HEAD)"
+docker exec dummy touch "/root/$(git rev-parse HEAD)"
 docker stop dummy
