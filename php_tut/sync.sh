3a3666c39498543c298c95ec070cf36299e17da7
Kartik Gokte <ksgokte@gmail.com>
2020-12-11 12:08:12 +0530

    Updated db location

--- php_tut/sync.sh
+++ php_tut/sync.sh
@@ -3,7 +3,7 @@
 set -ex
 echo $PWD
 docker run -d --rm --name dummy -v xampp-pma-volume:/root alpine tail -f /dev/null
-docker cp ./code dummy:/root/
+docker -D cp ./code dummy:/root/
 docker exec dummy rm "/root/$(git rev-parse HEAD~)"
 docker exec dummy touch "/root/$(git rev-parse HEAD)"
 docker stop dummy
