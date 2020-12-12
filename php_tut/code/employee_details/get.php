c87bdead0696095c7d7262c916cce7b8edd50524
Kartik Gokte <ksgokte@gmail.com>
2020-12-12 19:14:51 +0530

    RCA

--- php_tut/code/employee_details/get.php
+++ php_tut/code/employee_details/get.php
@@ -165,7 +165,7 @@ $elapsed_time = microtime(true) - $start_time;
                         echo "Empty set<br>";
                     }
                     else {
-                        echo '<table class="table table-bordered table-striped table-hover">';
+                        echo '<table class="table table-bordered ">';
                         echo '<thead class="thead-light>';
                         echo '<tr>';
                         echo '<th scope="col">No.</th>';
@@ -189,36 +189,7 @@ $elapsed_time = microtime(true) - $start_time;
                     }
                 }
                 ?>
-                <table class="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+                
             </div>
             <div class="col-sm-2 sidenav">
                 <div class="jumbotron bg-warning">
