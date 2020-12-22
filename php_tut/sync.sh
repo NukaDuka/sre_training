3719732ba14990631c934bc1406d6aa295bd3174
Kartik Gokte <ksgokte@gmail.com>
2020-12-23 00:15:17 +0530

    Updated pipeline sync to be more robust

--- php_tut/sync.sh
+++ php_tut/sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+docker stop dummy
 set -ex
-echo $PWD
 docker run -d --rm --name dummy -v xampp-pma-volume:/root alpine tail -f /dev/null
 docker -D cp ./code dummy:/root/
-docker exec dummy rm "/root/$(git rev-parse HEAD~)"
+docker exec dummy rm -f "/root/$(git rev-parse HEAD~)"
 docker exec dummy touch "/root/$(git rev-parse HEAD)"
 docker stop dummy
