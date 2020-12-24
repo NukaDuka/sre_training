8fdde7954e84fd0978890939b55a4ffee21cf845
Kartik Gokte <ksgokte@gmail.com>
2020-12-24 22:18:40 +0530

    Added addn checks

--- php_tut/code/employee_details/edit.php
+++ php_tut/code/employee_details/edit.php
@@ -32,12 +32,9 @@ if (isset($_POST["submit"])) {
     }
     $query_string .= " where id=?";
     $query = mysqli_prepare($con, $query_string);
-    if ($_POST['empName'] != "") mysqli_stmt_bind_param($query, 'ss', $id, $name);
-    else if ($_POST['empPos'] != "") mysqli_stmt_bind_param($query, 'ss', $id,  $pos);
+    if ($_POST['empName'] != "" && $_POST['empPos'] == "") mysqli_stmt_bind_param($query, 'ss', $id, $name);
+    else if ($_POST['empPos'] != "" && $_POST['empName'] == "") mysqli_stmt_bind_param($query, 'ss', $id,  $pos);
     else if ($_POST['empName'] != "" && $_POST['empPos'] != "") mysqli_stmt_bind_param($query, 'sss', $id,  $name, $pos);
-    else{
-        
-    }
 
     if (!mysqli_stmt_execute($query)) {
         redirect("/php_tut/code/employee_details/error.php");
