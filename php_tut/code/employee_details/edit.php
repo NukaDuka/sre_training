bb64ff0f5df277ad87711002b2eb36130c72806a
Kartik Gokte <ksgokte@gmail.com>
2020-12-24 19:02:49 +0530

    Added delete and edit pages

---
+++ php_tut/code/employee_details/edit.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+        }
+    </style>
+    <style>
+        /* Remove the navbar's default margin-bottom and rounded borders */
+        .navbar {
+          margin-bottom: 0;
+          border-radius: 0;
+        }
+
+        /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+        .row.content {height: 100%}
+
+        .sidenav {
+          padding-top: 20px;
+          background-color: #f1f1f1;
+          height: 100%;
+        }
+
+        footer {
+          background-color: #555;
+          color: white;
+          padding: 15px;
+        }
+
+        @media screen and (max-width: 767px) {
+          .sidenav {
+            height: auto;
+            padding: 15px;
+          }
+          .row.content {height:auto;}
+        }
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    <title>Employee details homepage</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #e3f2fd;">
+        <a class="navbar-brand" href="index.html">Employee details page</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home</a></li>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="enter.php">Enter details</a></li>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="get.php">Get details</a></li>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <br>
+    <div class="container-fluid text-center">
+        <div class="row content">
+            <div class="col-sm-2 sidenav">
+                <div class="jumbotron bg-danger">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                <div class="jumbotron bg-danger">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                <div class="jumbotron bg-danger">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                <div class="jumbotron bg-danger">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                <div class="jumbotron bg-danger">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                <div class="jumbotron bg-danger">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+            </div>
+            <div class="col-sm-8 text-left">
+                <h1>Your new administrative space</h1>
+                <p>Using our revolutionary new proprietary database entry technology, updating your employee details has never been easier!</p>
+                <hr>
+                <h3>Sitemap</h3>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><a href="index.html">Home</a></li>
+                    <li class="list-group-item"><a href="enter.php">Enter details</a></li>
+                    <li class="list-group-item"><a href="get.php">Get details</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-2 sidenav">
+                <div class="jumbotron bg-warning">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                <div class="jumbotron bg-warning">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                <div class="jumbotron bg-warning">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                <div class="jumbotron bg-warning">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                <div class="jumbotron bg-warning">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                <div class="jumbotron bg-warning">
+                    <p>INTRUSIVE ADS</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="container-fluid text-center">
+        <p>Copyright &copy; 2020 ROFL Inc. All rights reserved</p>
+    </footer>
+</body>
+</html>
