9476ab37c5d62276fc0c338e4152e395b966f921
Kartik Gokte <ksgokte@gmail.com>
2020-12-24 17:46:29 +0530

    Added debug string

--- php_tut/code/employee_details/enter.php
+++ php_tut/code/employee_details/enter.php
@@ -19,7 +19,8 @@
         mysqli_close($con);
         $header_str = "Location: /php_tut/code/employee_details/get.php?redirect=true&empID=" . $id;
         header($header_str); 
-        exit();
+        echo $success;
+        #exit();
     }
 ?>
 
