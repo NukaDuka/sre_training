b3aea6c636dd4c45a0454d13659f752019b92675
Kartik Gokte <ksgokte@gmail.com>
2020-12-24 23:22:29 +0530

    Changed CSS

--- php_tut/code/employee_details/delete.php
+++ php_tut/code/employee_details/delete.php
@@ -126,7 +126,7 @@
                         <li class="list-group-item">Employee name: <?php echo $name ?></li>
                         <li class="list-group-item">Position: <?php echo $pos ?></li>
                     </ul>   
-                    <div class="row align-items-center">
+                    <div class="row align-self-center">
                         <input type="submit" class="btn btn-danger" id="no" name="no" value="No">
                         <input type="submit" class="btn btn-dark" id="yes" name="yes" value="Yes">
                     </div>                     
