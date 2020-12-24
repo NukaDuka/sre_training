f841b3d4882e1686ef4cf7685a9e5fb4bb88b732
Kartik Gokte <ksgokte@gmail.com>
2020-12-24 23:12:58 +0530

    Changed CSS

--- php_tut/code/employee_details/delete.php
+++ php_tut/code/employee_details/delete.php
@@ -117,17 +117,17 @@
                 </div>
             </div>
             <div class="col-sm-8">
-                <div class="text-left"><h1>Delete employee record</h1></div>
+                <div class="text-left row"><h1>Delete employee record</h1></div>
                 <hr>
                 <h3>Are you sure you want to delete the following record?</h3>
-                <div class="justify-content-center">
+                <div class="justify-content-center row">
                     <ul class="list-group">
                         <li class="list-group-item">Employee ID: <?php echo $id ?></li>
                         <li class="list-group-item">Employee name: <?php echo $name ?></li>
                         <li class="list-group-item">Position: <?php echo $pos ?></li>
                     </ul>                        
                 </div>
-                <div class="justify-content-center">
+                <div class="justify-content-center row">
                     <input type="submit" class="btn btn-danger" id="no" name="no" value="No">
                     <input type="submit" class="btn btn-dark" id="yes" name="yes" value="Yes">
                 </div>
