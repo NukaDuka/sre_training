8f567d381348b6671b3332957c47f88180c65b7e
Kartik Gokte <ksgokte@gmail.com>
2020-12-24 22:47:48 +0530

    Added linebreak

--- php_tut/code/employee_details/delete.php
+++ php_tut/code/employee_details/delete.php
@@ -121,6 +121,7 @@
                 <hr>
                 <div class="d-flex justify-content-center">
                     <h3>Are you sure you want to delete the following record?</h3>
+                    <br>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Employee ID: <?php echo $id ?></li>
                         <li class="list-group-item">Employee name: <?php echo $name ?></li>
