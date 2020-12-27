ac992dfded5919b27d760d0540ce51208a81a046
Kartik Gokte <ksgokte@gmail.com>
2020-12-27 10:36:33 +0530

    Updated build script

--- php_tut/homebrew-xampp/phpMyAdmin-custom/build_and_push.sh
+++ php_tut/homebrew-xampp/phpMyAdmin-custom/build_and_push.sh
@@ -1,5 +1,5 @@
 set -x
-docker build -t nukaduka1/custom-xampp .
+docker build --rm -t nukaduka1/custom-xampp .
 docker tag nukaduka1/custom-xampp nukaduka1/custom-xampp:latest
 docker login
 docker push nukaduka1/custom-xampp:latest
