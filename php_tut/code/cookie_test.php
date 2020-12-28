654a351a08c01e4c02ef5a4f4351e44be3a58c49
Kartik Gokte <ksgokte@gmail.com>
2020-12-28 12:29:27 +0530

    Added header code

--- php_tut/code/cookie_test.php
+++ php_tut/code/cookie_test.php
@@ -1,6 +1,18 @@
 <?php
 header("Cache-Control: no-cache");
+if (!isset($_COOKIE['sessionID'])){
+    $token = md5(strval(microtime()));
+    $token_header = "Set-Cookie: sessionID=" . $token . "; Max-age=120";
+    header($token_header);
+    echo "<h1>Welcome new user!</h1>";
+    echo "<h3>Your token number is " . $token . "</h3>";
+}
+else {
+    $token = $_COOKIE['sessionID'];
+    echo "<h1>Welcome back, user!</h1>";
+    echo "<h3>Your token number is " . $token . "</h3>";
+}
 print_r($_COOKIE);
-$token = md5(strval(microtime()));
+
 echo "<br>" . $token;
 ?>
