d9aa4a272652b0d1b18123b959de984d774cde22
Kartik Gokte <ksgokte@gmail.com>
2020-12-29 18:43:40 +0530

    Changed CSS

--- php_tut/code/top-secret-login-page/index.php
+++ php_tut/code/top-secret-login-page/index.php
@@ -7,6 +7,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+
+    </style>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
@@ -14,7 +17,7 @@
     <title>TOP SECRET login</title>
 </head>
 <body>
-    <div class="w-100 h-100">
+    <div class="w-100 vh-100">
         <div class="d-flex justify-content-center align-items-center flex-fill-1">
             <p class="text-center">test</p>
         </div>
