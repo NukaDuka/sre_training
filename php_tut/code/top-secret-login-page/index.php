81a57798d7d253b88ce0129659f9837358791b9e
Kartik Gokte <ksgokte@gmail.com>
2021-01-14 22:48:55 +0530

    Changed form elements

--- php_tut/code/top-secret-login-page/index.php
+++ php_tut/code/top-secret-login-page/index.php
@@ -47,7 +47,7 @@
                         </label>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
+                <button type="submit" name="submit" id="submit" class="btn btn-primary">Sign in</button>
             </form>
         </div>
     </div>
