afd9806e2dc36a278037424b2932d75422a91d79
Kartik Gokte <ksgokte@gmail.com>
2021-01-14 23:16:46 +0530

    Changed CSS

--- php_tut/code/top-secret-login-page/index.php
+++ php_tut/code/top-secret-login-page/index.php
@@ -47,9 +47,9 @@
                         </label>
                     </div>
                 </div>
-                <button type="submit" name="submit" id="submit" class="btn btn-primary">Sign in</button>
+                <button type="submit" name="submit" id="submit" class="btn btn-primary">Sign in</button> <a href="new.php">Create new account</a>
             </form>
-            <div class="h-100 w-100 container"><div class="row align-items-center"><h5 class="text-center "><small class="muted"><a href="new.php">Create new account</a></small></h5></div></div>
+            <a href="new.php">Create new account</a>
             
         </div>
     </div>
