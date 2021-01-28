9081e5d8f2503976428742b36377005845971489
Kartik Gokte <ksgokte@gmail.com>
2021-01-29 00:33:16 +0530

    Changed debug stuff

--- php_tut/code/top-secret-login-page/validate.php
+++ php_tut/code/top-secret-login-page/validate.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 $redis = new Redis();
-//$redis->connect('redis', 6379, 1, NULL, 0, 0, ['auth' => ['ts-redis', 'dGVzdHBhc3N3ZAo']]);
+$redis->connect('redis', 6379, 1, NULL, 0, 0, ['auth' => ['ts-redis', 'dGVzdHBhc3N3ZAo']]);
 //echo $redis->ping('test');
 //$key = $redis->get('ts_admin:token_key');
 //$_SESSION['key'] = $key;
