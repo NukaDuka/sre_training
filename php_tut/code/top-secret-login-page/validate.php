f305b221f076927bb7523a637e417279321efe19
Kartik Gokte <ksgokte@gmail.com>
2021-01-29 00:40:56 +0530

    Removed debug stuff

--- php_tut/code/top-secret-login-page/validate.php
+++ php_tut/code/top-secret-login-page/validate.php
@@ -2,17 +2,11 @@
 session_start();
 $redis = new Redis();
 $redis->connect('redis', 6379, 1, NULL, 0, 0, ['auth' => ['ts-redis', 'dGVzdHBhc3N3ZAo']]);
-try {
-    $_SESSION['ping'] = $redis->ping('test');
-}
-catch(Exception $e) {
-    $_SESSION['error'] = $e->getMessage();
-}
-//$key = $redis->get('ts_admin:token_key');
-//$_SESSION['key'] = $key;
-//$token = hash('sha256', uniqid(session_id(), true));
-//$cookie = hash_hmac('sha256', json_encode(array("uname"=>$_POST['uname'], "token"=>$token)), $key);
-//$_SESSION['cook'] = $cookie;
+key = $redis->get('ts_admin:token_key');
+$_SESSION['key'] = $key;
+$token = hash('sha256', uniqid(session_id(), true));
+$cookie = hash_hmac('sha256', json_encode(array("uname"=>$_POST['uname'], "token"=>$token)), $key);
+$_SESSION['cook'] = $cookie;
 if (!isset($_POST['submit'])) {
     header('Location: /php_tut/code/top-secret-login-page/index.php');
     exit();
