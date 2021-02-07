dc2e9b531670ced76dfbd73efedb84f503060ac8
Kartik Gokte <ksgokte@gmail.com>
2021-02-07 18:59:13 +0530

    Added decryption code

--- php_tut/code/top-secret-login-page/index.php
+++ php_tut/code/top-secret-login-page/index.php
@@ -5,7 +5,12 @@ session_start();
 if (isset($_COOKIE['ts_auth']))
 {
     $cookie = $_COOKIE['ts_auth'];
-    echo $cookie;
+    $redis = new Redis();
+    $redis->connect('redis', 6379, 1, NULL, 0, 0, ['auth' => ['ts-redis', 'dGVzdHBhc3N3ZAo']]);
+    $key = $redis->get('ts_admin:token_key');
+    $iv = $redis->get('ts_admin:iv');
+    $message = openssl_decrypt($cookie, 'AES-128-CTR', $key, 0, $iv);
+    echo $message;
 }
 session_destroy();
 ?>
--- php_tut/code/top-secret-login-page/new.php
+++ php_tut/code/top-secret-login-page/new.php
@@ -71,7 +71,7 @@ session_destroy()
                         <input type="password" id="cpasswd" name="cpasswd" class="form-control" onkeyup='check();' placeholder="Password" autocomplete="off" required>
                     </div>
                 </div>
-               <button type="submit" name="submit" id="submit" class="btn btn-primary" disabled>Sign in</button>
+               <button type="submit" name="submit" id="submit" class="btn btn-primary" disabled>Create Account</button>
             </form>
             
         </div>
