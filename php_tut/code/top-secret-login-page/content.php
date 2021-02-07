c7426a1790f6c40f760bfbb02654837a6dd81153
Kartik Gokte <ksgokte@gmail.com>
2021-02-07 19:12:12 +0530

    Added login verification

--- php_tut/code/top-secret-login-page/content.php
+++ php_tut/code/top-secret-login-page/content.php
@@ -1,3 +1,24 @@
 <?php
-//verify auth cookie and show content
+session_start();
+if (isset($_COOKIE['ts_auth']))
+{
+    $cookie = $_COOKIE['ts_auth'];
+    $redis = new Redis();
+    $redis->connect('redis', 6379, 1, NULL, 0, 0, ['auth' => ['ts-redis', 'dGVzdHBhc3N3ZAo']]);
+    $key = $redis->get('ts_admin:token_key');
+    $iv = $redis->get('ts_admin:iv');
+    $message = openssl_decrypt($cookie, 'AES-128-CTR', $key, 0, $iv);
+    $json_message = json_decode($message);
+    if (!hash_equals($json_message->token, $redis->get('ts:' . $json_message->uname))){
+        setcookie('ts_auth', "", time() - 3600);
+        header('Location: /php_tut/code/top-secret-login-page/index.php');
+        exit();
+    }
+}
+else 
+{
+    header('Location: /php_tut/code/top-secret-login-page/index.php');
+    exit();
+}
+session_destroy();
 ?>
--- php_tut/code/top-secret-login-page/index.php
+++ php_tut/code/top-secret-login-page/index.php
@@ -4,14 +4,8 @@
 session_start();
 if (isset($_COOKIE['ts_auth']))
 {
-    $cookie = $_COOKIE['ts_auth'];
-    $redis = new Redis();
-    $redis->connect('redis', 6379, 1, NULL, 0, 0, ['auth' => ['ts-redis', 'dGVzdHBhc3N3ZAo']]);
-    $key = $redis->get('ts_admin:token_key');
-    $iv = $redis->get('ts_admin:iv');
-    $message = openssl_decrypt($cookie, 'AES-128-CTR', $key, 0, $iv);
-    $json_message = json_decode($message);
-    if (hash_equals($json_message->token, $redis->get('ts:' . $json_message->uname))) echo 'we good';
+    header('Location: /php_tut/code/top-secret-login-page/content.php');
+    exit();
 }
 session_destroy();
 ?>
