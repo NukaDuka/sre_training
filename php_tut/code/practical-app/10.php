b79a816f81095a233392b72e534e9a02a5c53efa
Kartik Gokte <ksgokte@gmail.com>
2021-04-11 19:30:15 +0530

    Added ex10 stuff

--- php_tut/code/practical-app/10.php
+++ php_tut/code/practical-app/10.php
@@ -13,21 +13,41 @@
 
 	
 	<?php  
-
-	/*  Step 1: Use the Make a class called Dog
-
-		Step 2: Set some properties for Dog, Example, eye colors, nose, or fur color
-
-		Step 4: Make a method named ShowAll that echos all the properties
-
-		Step 5: Instantiate the class / create object and call it pitbull
-
-Step 6: Call the method ShowAll
-
 	
+	interface Animal 
+	{
+		public function ShowAll();
+	}
 
-		
-	*/
+	class Dog implements Animal
+	{
+		private $name
+		private $eye_color;
+		private $type;
+		private $fur_color;
+
+		function __construct($name, $eye_color, $type, $fur_color) 
+		{
+			$this->name = $name;
+			$this->eye_color = $eye_color;
+			$this->type = $type;
+			$this->fur_color = $fur_color;
+		}
+
+		function __destruct()
+		{
+			echo "apology for poor english<br>where were you when " . $this->name . " the " . $this->type . "dies?<br>i was at home eating Maggi 2-Minute Noodles Masala, 70g (Pack of 12) when pjotr ring<br>'" . $this->name . "is kill'<br>'no'";
+		}
+
+		public function ShowAll()
+		{
+			echo "Dog details:<br>Dog name: " . $this->name . "<br>Dog eye color: " . $this->eye_color . "<br>Dog type: " . $this->type . "<br>Dog fur color" . $this->fur_color . "<br>";
+		}
+	}
+	
+	$pitbull = new Dog("Buster", "Brown", "Pitbull", "Light Gray");
+	$pitbull->ShowAll();
+	unset($pitbull);
 	
 	?>
 
